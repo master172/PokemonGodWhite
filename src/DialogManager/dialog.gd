00cac69c70b31ac79d0c08096309fb24f25756ae
master172 <80324139+master172@users.noreply.github.com>
2023-07-06 18:43:48 +0530

    small dialog fix

--- src/DialogManager/dialog.gd
+++ src/DialogManager/dialog.gd
@@ -4,7 +4,9 @@ extends Control
 
 @export var current_dialog:DialogueLine
 
-var current_index = 0
+var current_index:int = 0
+var tween
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_text_empty()
@@ -13,27 +15,38 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("Yes"):
-		process_dialog()
+		if DialogDisplay.get_visible_ratio() == 1.0:
+			process_dialog()
+		else:
+			skip_display_animation()
 
 func set_text_empty():
 	DialogDisplay.set_visible_characters(0)
 	DialogDisplay.text = ""
 	
 func displayText(text):
+	print("hi")
 	DialogDisplay.text = text
 	await get_tree().create_timer(0.2).timeout
 	
-	var tween = get_tree().create_tween()
+	tween = get_tree().create_tween()
 	tween.tween_property(DialogDisplay, "visible_ratio", 1, 1)
 
+func skip_display_animation():
+	if is_instance_valid(tween):
+		tween.stop()
+	DialogDisplay.set_visible_ratio(1)
+	
 func process_dialog():
 	if current_dialog != null:
 		if current_dialog.Dialogs.size() >= 1:
-			if current_index <= (current_dialog.Dialogs.size() -1):
+			if current_index >= 0:
 				show()
 				set_text_empty()
 				displayText(current_dialog.Dialogs[current_index].text)
-				current_index += 1
-			else:
+					
+				current_index = current_dialog.Dialogs[current_index].next
+				
+			elif current_index == -1:
 				set_text_empty()
 				hide()
