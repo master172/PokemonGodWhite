0080f5e118d93d547a094d4818f8b0f63d91463a
master172 <80324139+master172@users.noreply.github.com>
2023-10-14 13:29:24 +0530

    add thunder bolt sfx

--- ThunderBolt.gd
+++ ThunderBolt.gd
@@ -1,11 +1,21 @@
 extends Action
 
+@export var attack_pos := Vector2(0,0)
+
+@onready var beam = $Beam
+@onready var electric_1 = $Electric1
+@onready var electric_2 = $Electric2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+#func _physics_process(delta):
+#	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+#		attack_pos = get_global_mouse_position()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	beam.look_at(attack_pos)
+	beam.scale.x = (self.global_position.distance_to(attack_pos))/128
+	electric_2.global_position = attack_pos
