457b966304da48925109fa1c1cd9f00d2b09042d
master172 <80324139+master172@users.noreply.github.com>
2023-10-28 12:44:58 +0530

    Update SurfChecker.gd

--- src/player/ash/SurfChecker.gd
+++ src/player/ash/SurfChecker.gd
@@ -16,7 +16,29 @@ func process_tilemap_collision(player:Node2D,check:String):
 	
 	if current_tilemap != null:
 	
-		var tile_cords = current_tilemap.local_to_map(position)
+		
+		var node_global_position = global_position
+
+		var tilemap_global_position = current_tilemap.global_position
+
+		
+		var offset = node_global_position - tilemap_global_position
+
+		
+		var tilemap_offset = current_tilemap.position
+
+		
+		var tilemap_coord = offset - tilemap_offset
+
+		
+		var tile_size = Vector2(16, 16)
+
+	
+		var tile_cords = offset / tile_size
+
+		print(tile_cords)
+		tile_cords = tile_cords.round()
+		print(tile_cords)
 		
 		var sucess =  [true,position,tile_cords]
 		var faliure = [false,position,tile_cords]
