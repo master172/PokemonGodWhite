99e50a693fa49d5ffc5d31d077f7d85a1ea71ee3
master172 <80324139+master172@users.noreply.github.com>
2023-11-01 18:08:35 +0530

    add peck

---
+++ Core/Actions/Peck.gd
@@ -0,0 +1,49 @@
+extends Action
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
+var User:CharacterBody2D = null
+
+var oneshot:bool = false
+
+func set_user(user):
+	User = user
+
+func _attack():
+	animated_sprite_2d.play("Attack")
+	if User != null:
+		var direction = User.get_current_facing_direction()
+		self.position += direction * 48
+		position -= Vector2(0,16)
+		if User.is_in_group("Pokemon"):
+			var target = User.targetPokemon
+			if target != null:
+				look_at(target.global_position)
+		elif User.is_in_group("PlayerPokemon"):
+			
+		
+			if direction == Vector2(0,-1):
+				self.rotation_degrees = 180
+			elif direction == Vector2(0,1):
+				self.rotation_degrees = 0
+			elif direction  == Vector2(-1,0):
+				self.rotation_degrees = 90
+			elif direction == Vector2(1,0):
+				self.rotation_degrees = -90
+		User.velocity = User.velocity * 0.001
+		
+func _on_animated_sprite_2d_animation_finished():
+	if User.has_method("attack_end"):
+		User.attack_end()
+	connect("attack_finished",SignalBus.attack_completed)
+	emit_signal("attack_finished",self,User)
+	queue_free()
+
+
+func _on_area_2d_body_entered(body):
+	if body != User:
+		if body.is_in_group("Pokemon") or body.is_in_group("PlayerPokemon"):
+			holder.calculate_damage(body,User)
+			if oneshot == false:
+				connect("attack_landed",SignalBus.on_attack_landed)
+				oneshot = true
+			emit_signal("attack_landed",self,User)
