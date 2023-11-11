5d674dc9dea40f705b8ff5c718a37ca175568542
master172 <80324139+master172@users.noreply.github.com>
2023-11-11 16:43:22 +0530

    add stun spore
    kinda overpowered without stamina

---
+++ Core/Actions/StunSpore.gd
@@ -0,0 +1,44 @@
+extends Action
+
+
+
+var target:Array = []
+var User:CharacterBody2D
+
+
+var oneshot:bool = false
+
+func _ready():
+	pass
+
+func set_user(user):
+	User = user
+
+func set_target(Target):
+	target = Target
+	
+	
+
+func _on_timer_timeout():
+	_end()
+	queue_free()
+
+
+func _attack():
+	if target != null:
+		if User.opposing_pokemons != []:
+			if User.opposing_pokemons[0] == target[0]:
+				print("attack")
+				target[0].animate_modulation_change(Color.YELLOW,5)
+				target[0].stun(5)
+				if oneshot == false:
+					connect("attack_landed",SignalBus.on_attack_landed)
+					oneshot = true
+				emit_signal("attack_landed",self,User)
+
+func _end():
+
+	if User.has_method("attack_end"):
+		User.attack_end()
+	connect("attack_finished",SignalBus.attack_completed)
+	emit_signal("attack_finished",self,User)
