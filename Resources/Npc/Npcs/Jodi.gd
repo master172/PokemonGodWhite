135931304436da1bc575c9346484cc6d9c3d96ec
master172 <80324139+master172@users.noreply.github.com>
2023-11-19 11:47:03 +0530

    fix jodi

--- Resources/Npc/Npcs/Jodi.gd
+++ Resources/Npc/Npcs/Jodi.gd
@@ -3,12 +3,12 @@ extends looking_trainer
 func _ready():
 	basic_set()
 	looking_set()
-	if Utils.jodi_defeated == true:
+	if Utils.Jodi_defeated == true:
 		self.current_dialog = self.ending_dialog
 		
 func my_battle_finished():
 	if my_battle == true:
-		Utils.jodi_defeated = true
+		Utils.Jodi_defeated = true
 		my_battle_done = true
 		self.current_dialog = self.ending_dialog
 		emit_signal("battle_done")
