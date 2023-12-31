4b60c645ce842a9d5e75b9e4690322c9eddee9bb
master172 <80324139+master172@users.noreply.github.com>
2023-12-31 18:30:27 +0530

    add route 2 initials

---
+++ src/World/Route2/CameraManager.gd
@@ -0,0 +1,23 @@
+extends AnimationPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_area_2d_body_entered(body):
+	play("area1")
+
+
+func _on_area_2d_2_body_entered(body):
+	play("area2")
+
+
+func _on_area_2d_3_body_entered(body):
+	play("area3")
