e4ab928e128875ca931a7bf906fd2225dd8055f3
master172 <themasterprogrammer70000@gmail.com>
2025-07-27 10:43:00 +0530

    Update scroll_container.gd

--- src/Ui/Pokedex/scroll_container.gd
+++ src/Ui/Pokedex/scroll_container.gd
@@ -26,4 +26,4 @@ func _input(event: InputEvent) -> void:
 		if event.is_action_pressed("S"):
 			scroll_vertical  = (scroll_vertical + 36) %36*max_scroll
 		elif event.is_action_pressed("W"):
-			scroll_vertical  = (scroll_vertical - 36) %36*max_scroll
+			scroll_vertical  = (scroll_vertical +35*max_scroll) %36*max_scroll
